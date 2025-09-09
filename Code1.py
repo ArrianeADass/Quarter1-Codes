a14578da54aa1b4d683370941b864a2cb2c4829e
ArrianeADass <arriane.dass@smc.pshs.edu.ph>
2025-09-09 10:33:24 +0800

    Add files via upload

---
+++ Code1.py
@@ -0,0 +1,8 @@
+#Identify num1 2 and 3
+num1 = float(input("Please enter your first number:"))
+num2 = float(input("Please enter your second number:"))
+num3 = float(input("Please enter your third number:"))
+
+#add num123
+ans = num1 + num2 + num3
+print(round(ans, 2))
